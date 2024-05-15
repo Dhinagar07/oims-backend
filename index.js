66d40303c9929359d12ccbbe944e0d85ac620652
Dhinagar07 <156648796+Dhinagar07@users.noreply.github.com>
2024-05-15 22:28:33 +0530

    first

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const createCustomer = require("./routes/create_customer")
+const adminLogin = require("./routes/login_route")
+const Product = require("./routes/product_route")
+const Order = require("./routes/order_route")
+
+
+const app = express();
+const port = 3001;
+
+app.use(bodyParser.json());
+app.use(cors()); 
+app.use("/auth",createCustomer);
+app.use("/auth",adminLogin);
+app.use("/order",Order);
+app.use("/product",Product);
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
