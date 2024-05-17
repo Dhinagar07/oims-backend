6516a910bbb8df76c066fe4fb995b2c9dc6ca8f3
Dhinagar07 <156648796+Dhinagar07@users.noreply.github.com>
2024-05-17 21:56:33 +0530

    newcommit

---
+++ controller/product.js
@@ -0,0 +1,115 @@
+const { QueryTypes } = require('sequelize');
+
+class product
+{
+    static async createProduct(db,obj) {
+            
+        const productcheck = await db.Product.sequelize.query(
+            'SELECT * FROM Products WHERE name = :name',
+            {
+              replacements: { name: obj.name },
+              type: QueryTypes.SELECT,
+            }
+          );
+                  
+        if (productcheck.length != 0) {
+            return false;
+        }
+
+            console.log(obj); 
+            const product = await db.Product.sequelize.query(
+                'INSERT INTO Products (name, description, price_per_unit, unit, stock_quantity, image_path, createdAt, updatedAt) VALUES (:name,:description , :price_per_unit,:unit,:stock_quantity,:image_path,NOW(),NOW())',
+                {
+                  replacements: {
+                    name: obj.name,
+                    description: obj.description,
+                    price_per_unit: obj.price,
+                    unit:obj.unit,
+                    stock_quantity:obj.stock_quantity,
+                    image_path:obj.image_path
+
+                  },
+                  type: QueryTypes.INSERT
+                }
+              );
+              
+            console.log(product); 
+            
+            console.log(product.length);
+            return product;
+        
+    }
+    static async listProduct(db){
+        const [list] = await db.Product.sequelize.query('SELECT * FROM Products');
+        
+        return list;    
+
+    }
+
+    static async deleteProduct(db,id){
+        const productcheck = await db.Product.sequelize.query(
+            'SELECT * FROM Products WHERE product_id = :id',
+            {
+              replacements: { id: id },
+              type: QueryTypes.SELECT
+            }
+          );
+          console.log(productcheck.length);
+        if (productcheck.length == 0) {
+            return false;
+        }
+
+
+        const product = await db.Product.sequelize.query(
+            'DELETE FROM Products WHERE product_id = :id',
+            {
+              replacements: { id: id },
+              type: QueryTypes.DELETE
+            }
+          );
+            return true;
+        
+        
+        
+    }
+
+    static async updateProduct(db,obj) {
+        const productcheck = await db.Product.sequelize.query(
+            'SELECT * FROM Products WHERE product_id = :id',
+            {
+              replacements: { id: obj.product_id },
+              type: QueryTypes.SELECT,
+            }
+          );
+          console.log(productcheck.length);
+        if (productcheck.length == 0) {
+            return false;
+        }
+            console.log(obj); 
+            const product = await db.Product.sequelize.query(
+                'UPDATE Products SET name = :name, description=:description ,price_per_unit=:price_per_unit, unit=:unit, stock_quantity=:stock_quantity,updatedAt=NOW() WHERE product_id = :product_id',
+                {
+                  replacements: {
+                    name: obj.name,
+                    description: obj.description,
+                    price_per_unit: obj.price_per_unit,
+                    unit:obj.unit,
+                    stock_quantity:obj.stock_quantity,
+                    product_id:obj.product_id
+
+                  },
+                  type: QueryTypes.UPDATE
+                }
+              );
+            if(product.length !=0){
+                return true;
+            }
+            else{
+                return false;
+            }
+                    
+    }
+
+
+}
+module.exports=product;
---
+++ routes/product_route.js
@@ -0,0 +1,100 @@
+const express = require('express');
+const router = express.Router();
+const db= require('../models/db');
+const product=require('../controller/product');
+router.use(express.json())
+const multer = require('multer');
+const path = require('path');
+
+// Set up multer storage and file naming
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // Save files in the uploads folder
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname)); // Rename files with current timestamp
+  },
+});
+
+const upload = multer({ storage: storage });
+
+router.post('/create-product', upload.single('image'), async (req, res) => {
+  
+  try {
+    console.log(req.file);
+    console.log(req.body);
+    const imagePath = req.file ? 'uploads/' + req.file.filename : null;
+    req.body.image_path = imagePath;   
+  
+    result= await product.createProduct(db,req.body);
+    console.log('res',result);
+    if (result) {
+        res.status(200).json({ "message": "Successfully inserted" });
+    } else {
+        res.status(403).json({ "error": "Product exist already" });
+    }
+} 
+catch (error) {
+    
+    console.error('Error creating product : ', error);
+    res.status(500).json({ "error": "Internal server error " });
+}
+
+});
+
+
+// Route to get all products
+router.post('/list', async (req, res) => {
+    try {
+       const result= await product.listProduct(db);
+       
+        
+        if (!result) {
+            res.status(403).json({ "error": "Product not exist" });
+        } else {
+            res.status(200).json(result);                   }
+        
+    } catch (error) {
+        console.error('Error fetching products:', error);
+        res.status(500).json({" error": 'Failed to fetch products' });
+    }
+});
+
+
+router.put('/:id', async (req, res) => {
+    try {
+        const productId = parseInt(req.params.id);
+        console.log(req.body);
+        req.body.product_id = productId;
+        result= await product.updateProduct(db,req.body);
+        if(result)
+        {
+            res.status(200).json({"message": "Successfully updated"});
+
+        }else{
+            res.status(403).json({"message": "PRODUCT NOT EXIST"});
+        }
+    } catch (error) {
+        console.error('server Error :', error);
+        res.status(500).json({ "error": 'server error' });
+    }
+});
+router.delete('/:id', async (req, res) => {
+    try{
+    result= await product.deleteProduct(db,parseInt(req.params.id));
+    
+        if (result) {
+            res.json({ message: 'Row deleted successfully' });
+        } else {
+            res.status(403).json({ "error": "product not exist" });
+        }
+    } catch (error) {
+        console.error('server error:', error);
+        res.status(500).json({ "error": 'internal server error' });
+    }
+  });
+
+
+
+
+module.exports = router
